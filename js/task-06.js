6079aa25f77e82e5e955f0ba5561e49c5f7353d8
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-07-31 00:02:42 +0200

    error fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const textInput = document.querySelector("input");
 const dataLength = textInput.dataset.length;
 
 textInput.addEventListener("blur", () => {
-  if (Number(textInput.value.length) == dataLength) {
+  if (Number(textInput.value.length) === Number(dataLength)) {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
   } else {
