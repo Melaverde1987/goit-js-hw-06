d920621592f43e2ad8fc86dc910116e05903143f
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-07-30 23:52:21 +0200

    error fixed

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,10 @@ const dataLength = textInput.dataset.length;
 
 textInput.addEventListener("blur", () => {
   if (textInput.value.length === dataLength) {
-    textInput.classList.add("invalid");
-    textInput.classList.remove("valid");
-  } else {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
+  } else {
+    textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
   }
 });
