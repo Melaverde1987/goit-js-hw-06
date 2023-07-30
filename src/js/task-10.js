dae66c5de8dca23634e372e138115896039a3f17
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-07-30 03:27:35 +0200

    10 tasks

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  let markup = "";
+  let width = 30;
+  let height = 30;
+  let color = getRandomHexColor();
+
+  for (let i = 0; i < amount; i++) {
+    width += 10;
+    height += 10;
+    markup += `<div class="boxes-item" style="background-color: ${color}; width: ${width}px; height: ${height}px"></div>`;
+  }
+  return markup;
+}
+
+function destroyBoxes() {
+  let markup = "";
+  return markup;
+}
+
+const boxes = document.querySelector("#boxes");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+btnCreate.addEventListener("click", () => {
+  const inputValue = input.value;
+  boxes.insertAdjacentHTML("beforeend", createBoxes(inputValue));
+});
+
+btnDestroy.addEventListener("click", () => {
+  boxes.innerHTML = destroyBoxes();
+});
